8ffa4fbb3fbd31d61a4d7ba417645d2c65fd14f3
yuvraj-github <54491015+yuvraj-github@users.noreply.github.com>
2022-01-16 21:08:20 +0530

    auto deployment

---
+++ server_deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+echo "Deploying application ..."
+
+# Enter maintenance mode
+(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+    # Update codebase
+    git fetch origin deploy
+    git reset --hard origin/deploy
+
+    # Install dependencies based on lock file
+    composer install --no-interaction --prefer-dist --optimize-autoloader
+
+# Exit maintenance mode
+php artisan up
+
+echo "Application deployed!"
