6855204a6db144edb542335f986e53ec9224e589
yuvraj-github <54491015+yuvraj-github@users.noreply.github.com>
2022-01-26 12:23:45 +0530

    saved yml and layout file

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -21,6 +21,7 @@
 </head>
 
 <body>
+    
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="/">Releases</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
